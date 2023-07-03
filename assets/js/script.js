fda8ac9dc2fe81081f8575e4416e53dbfc9ef119
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-07-03 22:55:13 +0000

    Added functions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+/* Two different event listeners
+Wait for DOM to finish loading before running the Quiz and
+when user clicks button */
+
+document.addEventListener("DOMContentLoaded", function() {
+
+})
+
+/* Functions */
+
+function loadQuiz () {
+
+}
+
+function runQuiz () {
+
+}
+
+function checkAnswer () {
+
+}
+
+function provideAnswer () {
+
+}
+
+function provideScore () {
+
+}
+
+function provideProgress () {
+
+}
+
+function scoreReaction () {
+
+}
+
+function goHome () {
+
+}
