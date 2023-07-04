885078938b0b26c40537ef32e44b8370f9e8beb7
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-07-04 20:40:07 +0000

    Added images to assets for quiz

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,7 +1,7 @@
 const quiz = [
 
     {
-        face: '',
+        "face": "assets/images/eltonjohn.jpg",
         name: "Elton John",
         options: ["Gerry Savage", "Jamie Alvarez", "Reginald Dwight"],
         answer: "Reginald Dwight"
@@ -9,28 +9,28 @@ const quiz = [
     },
 
     {
-        face: '',
+        "face": "assets/images/adamant.jpg",
         name: "Adam Ant",
         options: ["Louie Chase", "Stuart Goddard", "Virgie Hanson"],
         answer: "Stuart Goddard"
     },
 
     {
-        face: '',
-        name :"Calvin Harris",
+        "face": "assets/images/calvinharris.jpg",
+        name: "Calvin Harris",
         options: ['Jeff Shields', "Adam Wiles", "Mason Reese"],
         answer: "Adam Wiles"
     },
 
     {
-        face: '',
+        "face": "assets/images/johnwayne.jpg",
         name:"John Wayne",
         options: ["Long Todd", "Alvin Byrd", "Marion Morrison"],
         answer: "Marion Morrison"
     },
 
     {
-        face: '',
+        "face": "assets/images/sting.jpg",
         name :'Sting',
         options: ["Gordon Sumner", "Otto Marsh", "Cedric Castro"],
         answer: "Gordon Sumner"
